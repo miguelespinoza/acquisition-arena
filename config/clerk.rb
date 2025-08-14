592f739a1e285da75280de8c009b4eb5ab46984a
Miguel Espinoza <espinozamiguel349@gmail.com>
2025-08-14 12:23:03 -0400

    configure clerk if CLERK_SECRET_KEY exists

--- config/clerk.rb
+++ config/clerk.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-Clerk.configure do |c|
-  c.secret_key = ENV['CLERK_SECRET_KEY']
-  c.logger = Logger.new(STDOUT)
+if ENV['CLERK_SECRET_KEY'].present?
+  Clerk.configure do |c|
+    c.api_key = ENV['CLERK_SECRET_KEY']
+    c.logger = Logger.new(STDOUT) if Rails.env.development?
+  end
 end
