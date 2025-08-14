a531360ed4edf3dcb3dc69d1c54fb0b13e2ea31c
Miguel Espinoza <espinozamiguel349@gmail.com>
2025-08-14 12:36:33 -0400

    Update clerk.rb

--- config/clerk.rb
+++ config/clerk.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-if ENV['CLERK_SECRET_KEY'].present?
-  Clerk.configure do |c|
-    c.api_key = ENV['CLERK_SECRET_KEY']
-    c.logger = Logger.new(STDOUT) if Rails.env.development?
-  end
+Clerk.configure do |c|
+  c.api_key = ENV['CLERK_SECRET_KEY']
+  c.logger = Logger.new(STDOUT) if Rails.env.development?
 end
