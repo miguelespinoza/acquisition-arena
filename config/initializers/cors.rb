dc0fb8db4015e3ff0e84b2a6dbbd61b537986cc2
Miguel Espinoza <espinozamiguel349@gmail.com>
2025-08-15 16:39:35 -0400

    cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    if Rails.env.production?
+      origins 'https://acquisition-arena.crafted.app'
+    else
+      origins 'http://localhost:5173',
+              'https://localhost:5173'
+    end
+    
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400  # 24 hours cache for preflight
+  end
+end
