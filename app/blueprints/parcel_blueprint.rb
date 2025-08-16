14d60745ce311e2cf7291d33a980e6ede3b0a97e
Miguel Espinoza <espinozamiguel349@gmail.com>
2025-08-16 06:58:48 -0400

    override monetary values

--- app/blueprints/parcel_blueprint.rb
+++ app/blueprints/parcel_blueprint.rb
@@ -3,5 +3,21 @@
 class ParcelBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :parcel_number, :city, :state, :property_features, :created_at, :updated_at
+  fields :parcel_number, :city, :state, :created_at, :updated_at
+  
+  # Transform property_features to format monetary values
+  field :property_features do |parcel|
+    features = parcel.property_features.dup
+    
+    # Override monetary values with formatted currency strings
+    if features['market_value']
+      features['market_value'] = ActionController::Base.helpers.number_to_currency(features['market_value'])
+    end
+    
+    if features['assessed_value']
+      features['assessed_value'] = ActionController::Base.helpers.number_to_currency(features['assessed_value'])
+    end
+    
+    features
+  end
 end
