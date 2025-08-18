690a4463068dfbb0e84951d5939ba5c67e79b11c
Miguel Espinoza <espinozamiguel349@gmail.com>
2025-08-18 06:23:02 -0400

    remove voice_settings

--- app/blueprints/persona_blueprint.rb
+++ app/blueprints/persona_blueprint.rb
@@ -3,5 +3,5 @@
 class PersonaBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :name, :description, :avatar_url, :characteristics, :characteristics_version, :elevenlabs_agent_id, :conversation_prompt, :voice_settings, :agent_created_at, :voice_id, :created_at, :updated_at
+  fields :name, :description, :avatar_url, :characteristics, :characteristics_version, :elevenlabs_agent_id, :conversation_prompt, :agent_created_at, :voice_id, :created_at, :updated_at
 end
